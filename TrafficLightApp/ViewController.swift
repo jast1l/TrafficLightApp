fc902a1ced44f33fb13324806cbe049f0fbaf8de
Jast1l <jast1l@icloud.com>
2021-03-23 13:29:59 +0300

    Adding outlets and actions for objects. Adding their properties to viewDidLoad

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RedView: UIView!
+    @IBOutlet weak var YellowView: UIView!
+    @IBOutlet weak var GreenView: UIView!
+    @IBOutlet weak var StartButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        RedView.layer.cornerRadius = 90
+        YellowView.layer.cornerRadius = 90
+        GreenView.layer.cornerRadius = 90
+        
+        StartButton.layer.cornerRadius = 10
+        
+        RedView.alpha = 0.3
+        YellowView.alpha = 0.3
+        GreenView.alpha = 0.3
+        
     }
 
-
+    @IBAction func StartButtonPressed(_ sender: Any) {
+    }
+    
 }
 
