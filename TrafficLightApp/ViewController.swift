9b11883ccc3df4dced8f15bf5ffadcbf0da65f39
Jast1l <jast1l@icloud.com>
2021-03-23 18:51:33 +0300

    Creating application logic

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -8,30 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var RedView: UIView!
     @IBOutlet weak var YellowView: UIView!
     @IBOutlet weak var GreenView: UIView!
     @IBOutlet weak var StartButton: UIButton!
     
+    private var counterStartButtonPressed = 0 {
+        didSet {
+            if oldValue == 3 {
+                counterStartButtonPressed = 1
+            }
+        }
+    }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        RedView.layer.cornerRadius = 90
-        YellowView.layer.cornerRadius = 90
-        GreenView.layer.cornerRadius = 90
-        
         StartButton.layer.cornerRadius = 10
         
         RedView.alpha = 0.3
         YellowView.alpha = 0.3
         GreenView.alpha = 0.3
-        
-    }
-
-    @IBAction func StartButtonPressed(_ sender: Any) {
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewDidLoad()
+        
+        RedView.layer.cornerRadius = RedView.frame.size.width / 2
+        YellowView.layer.cornerRadius = YellowView.frame.size.width / 2
+        GreenView.layer.cornerRadius = GreenView.frame.size.width / 2
+    }
+    
+    @IBAction func StartButtonPressed() {
+        StartButton.setTitle("NEXT", for: .normal)
+        counterStartButtonPressed += 1
+        changingViewColor()
+    }
+    
+    private func changingViewColor() {
+        switch counterStartButtonPressed {
+        case 1 :
+            GreenView.alpha = 0.3
+            RedView.alpha = 1
+        case 2 :
+            RedView.alpha = 0.3
+            YellowView.alpha = 1
+        case 3 :
+            YellowView.alpha = 0.3
+            GreenView.alpha = 1
+        default:
+            break
+        }
+    }
 }
 
